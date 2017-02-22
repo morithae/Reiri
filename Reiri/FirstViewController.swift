179b17d24db1a8be017b1e8ea5b0b6a30fedde58
Moritz <moritz@Moritzs-MacBook-Pro.local>
2017-02-22 15:12:43 +0100

    Initial Commit

---
+++ Reiri/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Reiri
+//
+//  Created by Moritz on 22.02.17.
+//  Copyright © 2017 Moritz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
